1200a26e89a08810c58bbcc7916d5c9988fe2592
David Soliz Garcia <dichosg@hotmail.com>
2019-04-04 22:41:49 -0400

    Added components for users and repositories

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListUsersComponent } from './users/list-users/list-users.component';
+import { ListRepositoriesComponent } from './repositories/list-repositories/list-repositories.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'users',
+    component: ListUsersComponent
+  },
+  {
+    path: 'users/:id/repositories',
+    component: ListRepositoriesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListUsersComponent } from './users/list-users/list-users.component';
+import { ListRepositoriesComponent } from './repositories/list-repositories/list-repositories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListUsersComponent,
+    ListRepositoriesComponent
   ],
   imports: [
     BrowserModule,
